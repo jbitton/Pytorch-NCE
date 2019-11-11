b3b5f59d7e5d6527054fcdfd02ff9e2ef880e468
Joanna Bitton <joanna.bitton@gmail.com>
2019-11-11 13:14:25 -0800

    remove `install_requires` from `setup.py`

--- setup.py
+++ setup.py
@@ -15,7 +15,4 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'torch >= 1.0.0',
-    ],
 )
